ff39163e6edbb0c6ce411ee74bae82a4aca1ac01
Madhura <madhura.bhujbal@gmail.com>
2020-09-09 16:01:33 +0100

    Add input field and styling code for it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello world</div>;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <input type="text" />
+      </div>
+    );
+  }
 }
-
-export default App;

cd13cd6d5c8e49c3ab979853500c59cbb190c2bf
Madhura <madhura.bhujbal@gmail.com>
2020-09-10 15:45:33 +0100

    Import and include Display component in App to display it on screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Display from "./components/Display";
 
 export default class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Display />
+      </div>
+    );
   }
 }

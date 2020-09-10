42aff51f389607fab80152f25930ab5a13ad2412
Madhura <madhura.bhujbal@gmail.com>
2020-09-10 15:38:27 +0100

    Remove the App components code from App file and place it in Display component

--- src/App.js
+++ src/App.js
@@ -4,26 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header>
-          <h3>To-do List</h3>
-        </header>
-        <main>
-          <li className="item-list">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-1">
-                  <input type="checkbox" className="checkbox" />
-                </div>
-                <div className="col-md-2">
-                  <input type="text" className="item" autoFocus />
-                </div>
-              </div>
-            </div>
-          </li>
-        </main>
-      </div>
-    );
+    return <div className="App"></div>;
   }
 }
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function Display() {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <h3>To-do List</h3>
+      </header>
+      <main>
+        <li className="item-list">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-1">
+                <input type="checkbox" className="checkbox" />
+              </div>
+              <div className="col-md-2">
+                <input type="text" className="item" autoFocus />
+              </div>
+            </div>
+          </div>
+        </li>
+      </main>
+    </div>
+  );
 }
 
 export default Display;

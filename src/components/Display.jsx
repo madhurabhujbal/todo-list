092a49eb33725e8f2b14fae43bd20515cb5e216d
Madhura <madhura.bhujbal@gmail.com>
2020-09-11 17:08:11 +0100

    Modify Display as class component and include list props for displaying it on screen

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { Component } from "react";
 
-function Display() {
-  return (
-    <div>
-      <main>
-        <li className="item-list">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-1">
-                <input type="checkbox" className="checkbox" />
-              </div>
-              <div className="col-md-2">
-                <input type="text" className="item" autoFocus />
+class Display extends Component {
+  render() {
+    const { list } = this.props;
+    return (
+      <div>
+        <main>
+          <li className="item-list">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-1">
+                  <input type="checkbox" className="checkbox" />
+                </div>
+                <div className="col-md-2">
+                  <input type="text" className="item" value={list} autoFocus />
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-      </main>
-    </div>
-  );
+          </li>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default Display;

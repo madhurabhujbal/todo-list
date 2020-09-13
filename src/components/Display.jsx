3467e92cbe0f3edef3486c10e87f3eacb94fe1c1
Madhura <madhura.bhujbal@gmail.com>
2020-09-13 09:37:02 +0100

    Remove code for rendering list item and replace it with component instead

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Component } from "react";
+import ListItem from "./ListItem";
 
 class Display extends Component {
   render() {
@@ -7,19 +8,7 @@ class Display extends Component {
     return (
       <div className="container">
         {list.map((listItem) => (
-          <div className="row" key={list.id}>
-            <div className="col-md-1">
-              <input type="checkbox" className="checkbox" />
-            </div>
-            <div className="col-md-2">
-              <input
-                type="text"
-                className="item"
-                value={listItem.value}
-                autoFocus
-              />
-            </div>
-          </div>
+          <ListItem />
         ))}
       </div>
     );

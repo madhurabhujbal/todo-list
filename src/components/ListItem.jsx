3ae082c9ada002d31efcb7d05709961873b61d66
Madhura <madhura.bhujbal@gmail.com>
2020-09-13 09:36:22 +0100

    Add code for rendering individual list item

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function ListItem() {
-  return <div></div>;
+  return (
+    <div className="row" key={list.id}>
+      <div className="col-md-1">
+        <input type="checkbox" className="checkbox" />
+      </div>
+      <div className="col-md-2">
+        <input type="text" className="item" value={listItem.value} autoFocus />
+      </div>
+    </div>
+  );
 }
 
 export default ListItem;

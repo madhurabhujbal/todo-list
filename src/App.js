83865e28b57507124815d058cc5d626f446c1ecd
Madhura <madhura.bhujbal@gmail.com>
2020-09-16 15:36:42 +0100

    Add code to add a new item to the list when add button is clicked

--- src/App.js
+++ src/App.js
@@ -9,16 +9,26 @@ class App extends Component {
   };
 
   addItem = (e) => {
-    // const list = [...this.state.list];
-    e.preventDefault();
-    let itemName = e.target.value;
-    console.log(itemName);
+    // let itemName = e.target.value;
+    // console.log(id);
+    // this.state.list[id].value = itemName;
+    // const newList = [...this.state.list, { id, value: itemName }];
     //this.state.value = itemName;
-    this.setState((prevState) => {
-      console.log(prevState);
-      return { ...prevState, value: itemName };
-    });
+    // this.setState((prevState) => {
+    //   console.log(prevState);
+    //   return { ...prevState, value: itemName };
+    // });
   };
+
+  addNewItem = () => {
+    let item = { id: 11, value: "new item" };
+    const list = [...this.state.list, item];
+    this.setState({ list });
+  };
+
+  // updateItem(id, newValue) {
+  //   //change id'th item in the list with the newValue.
+  // }
 
   render() {
     return (
@@ -30,6 +40,13 @@ class App extends Component {
           <li className="item-list">
             <Display list={this.state.list} addItem={this.addItem} />
           </li>
+          <input type="button" onClick={this.addNewItem} value="Add new Item" />
+          <br />
+          {/* <input
+            type="button"
+            onClick={this.updateItem(1, "new value for the item")}
+            value="Update existing Item"
+          /> */}
         </main>
       </div>
     );

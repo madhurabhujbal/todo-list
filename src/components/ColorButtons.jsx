e745c687626fd741822496b7ac61591b6853cd97
Madhura <madhura.bhujbal@gmail.com>
2020-10-06 14:43:16 +0100

    Give classname to only main div for all buttons

--- src/components/ColorButtons.jsx
+++ src/components/ColorButtons.jsx
@@ -2,28 +2,14 @@ import React from "react";
 
 function ColorButtons({ changeAppBackgroundColor }) {
   return (
-    <div>
-      <button className="red" onClick={() => changeAppBackgroundColor("red")}>
-        Red
-      </button>
+    <div className="color-buttons">
+      <button onClick={() => changeAppBackgroundColor("red")}>Red</button>
 
-      <button className="blue" onClick={() => changeAppBackgroundColor("blue")}>
-        Blue
-      </button>
+      <button onClick={() => changeAppBackgroundColor("blue")}>Blue</button>
 
-      <button
-        className="green"
-        onClick={() => changeAppBackgroundColor("green")}
-      >
-        Green
-      </button>
+      <button onClick={() => changeAppBackgroundColor("green")}>Green</button>
 
-      <button
-        className="yellow"
-        onClick={() => changeAppBackgroundColor("yellow")}
-      >
-        Yellow
-      </button>
+      <button onClick={() => changeAppBackgroundColor("yellow")}>Yellow</button>
     </div>
   );
 }

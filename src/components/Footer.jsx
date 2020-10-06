34a8407141d9d0c5396119fd2366a4d0d27284be
Madhura <madhura.bhujbal@gmail.com>
2020-10-06 14:42:22 +0100

    Convert Footer class into stateless functional component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,27 @@
 import ColorButtons from "./ColorButtons";
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class Footer extends Component {
-  render() {
-    const { changeAppBackgroundColor } = this.props;
-    return (
-      <div>
-        <ColorButtons changeAppBackgroundColor={changeAppBackgroundColor} />
-      </div>
-    );
-  }
+import React from "react";
+
+function Footer({ changeAppBackgroundColor }) {
+  return (
+    <div>
+      <ColorButtons changeAppBackgroundColor={changeAppBackgroundColor} />
+    </div>
+  );
 }
 
 export default Footer;
+
+// class Footer extends Component {
+//   render() {
+//     const { changeAppBackgroundColor } = this.props;
+//     return (
+//       <div>
+//         <ColorButtons changeAppBackgroundColor={changeAppBackgroundColor} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default Footer;

653c34c0825345a7decfa01bb9efc210e5871861
Madhura <madhura.bhujbal@gmail.com>
2020-10-06 16:43:25 +0100

    Rearrange component elements in a row

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import ColorButtons from "./ColorButtons";
-
 import React from "react";
 
 function Footer({ changeAppBackgroundColor }) {
   return (
-    <div>
-      <ColorButtons changeAppBackgroundColor={changeAppBackgroundColor} />
+    <div className="container">
+      <div className="row color-select">
+        <div className="col-md-5">
+          <h6>Select App color :</h6>
+        </div>
+
+        <div className="col-md-6">
+          <ColorButtons changeAppBackgroundColor={changeAppBackgroundColor} />
+        </div>
+      </div>
     </div>
   );
 }
